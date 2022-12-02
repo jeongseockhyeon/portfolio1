957b5c9c0e28a1ca42490ba9b619c07d13ad3de8
jeongseockhyeon <jeong9260@naver.com>
2022-12-02 13:57:45 +0900

    first commit

--- components/footer.js
+++ components/footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
       <footer className=" body-font">
         <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
           <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-            <span className="ml-3 text-xl">석현's 포트폴리오</span>
+            <span className="ml-3 text-xl">석현 포트폴리오</span>
           </a>
           <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
             © jeong9260@naver.com
